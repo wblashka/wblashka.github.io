8e23577523438923a2047e0ef02f59a2c26aa823
wblashka <136365658+wblashka@users.noreply.github.com>
2024-05-23 16:45:08 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,11 +1,13 @@
 export class ConditionNode {
-    constructor({name, type, children = [],column1,column2,value=null}) {
+    constructor({name, type, children = [],column1,column2,value=null, uniqueIDs}) {
         this.name = name;
         this.type = type;
         this.children = children;
         this.column1 = column1;
         this.column2 = column2;
         this.value = value;
+        this.ID = uniqueIDs.length;
+        uniqueIDs.push(this.ID)
     }
     
     addChild(child) {
