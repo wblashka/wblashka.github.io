5d4a1471bffef763af711afa578a525ca754e335
wblashka <136365658+wblashka@users.noreply.github.com>
2024-05-24 14:39:55 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -6,20 +6,39 @@ export class ConditionNode {
         this.column1 = column1;
         this.column2 = column2;
         this.value = value;
+        this.parent = null;
         this.ID = uniqueIDs.length;
         uniqueIDs.push(this.ID)
     }
     
     addChild(child) {
         this.children.push(child);
+        child.parent = this;
     }
 
     addParent(parent) {
         if (parent != null) {
             parent.addChild(this);
+            this.parent = parent;
         }
     }
 
+    removeParent() {
+        this.parent = null;
+    }
+
+    checkIfIsChild(otherNode){
+        if (this.children.includes(otherNode)){
+            return true;
+        } else if (this.children.length > 0) {
+            for (const child of this.children) {
+                child.checkIfIsChild(otherNode);
+            }
+        } else {
+            return false;
+        }
+    }
+    
     evaluatethis(){
         //Add code later
     }
